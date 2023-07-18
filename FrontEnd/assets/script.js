e5a50fa17ce300a51279395e6df0f75aee92b6fe
Mélanie Fauchard <melaniefauchard0@gmail.com>
2023-07-18 11:34:46 +0200

    Ajout des travaux à la galerie via l'API

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -8,57 +8,28 @@ fetch("http://localhost:5678/api/works")
         console.log("error", error);
     })
 
-const works = [
-    {
-        "image": "abajour-tahina.png",
-        "tagLine": "Abajour Tahina",
-    },
-    {
-        "image": "appartement-paris-v.png",
-        "tagLine": "Appartement Paris V",
-    },
-    {
-        "image": "restaurant-sushisen-londres.png",
-        "tagLine": "Restaurant Sushisen - Londres",
-    },
-    {
-        "image": "la-balisiere.png",
-        "tagLine": "Villa “La Balisiere” - Port Louis",
-    },
-    {
-        "image": "structures-thermopolis.png",
-        "tagLine": "Structures Thermopolis",
-    },
-    {
-        "image": "appartement-paris-x.png",
-        "tagLine": "Appartement Paris X",
-    },
-    {
-        "image": "le-coteau-cassis.png",
-        "tagLine": "Pavillon “Le coteau” - Cassis",
-    },
-    {
-        "image": "villa-ferneze.png",
-        "tagLine": "Appartement Paris XVIII",
-    },
-    {
-        "image": "appartement-paris-xviii.png",
-        "tagLine": "Appartement Paris XVIII",
-    },
-    {
-        "image": "bar-lullaby-paris.png",
-        "tagLine": "Bar “Lullaby” - Paris",
-    },
-    {
-        "image": "hotel-first-arte-new-delhi.png",
-        "tagLine": "Hotel First Arte - New Delhi",
-    },
-    
-];
-
 function displayWorks(works) {
-    const imageGallery  = document.querySelector(".gallery > img")
-    const textGallery = document.querySelector(".gallery > figcaption")
-    
+    for(let index = 0; index < works.length; index++) {
+        
+        const figure = works[index];
+        // Récupération de l'élément du Dom qui accueillera la galerie
+        const sectionGallery = document.querySelector(".gallery");
+        // Création d'une balise dédiée à une image de la gallerie
+        const worksElement = document.createElement("figure");
+        // Créaton des balises interne 
+        const imageElement = document.createElement("img");
+        imageElement.src = figure.imageUrl;
+        const titleElement = document.createElement("figcaption");
+        titleElement.innerHTML = figure.title;
+
+        // On ratache la balise figure à la section gallery
+        sectionGallery.appendChild(worksElement);
+        worksElement.appendChild(imageElement);
+        worksElement.appendChild(titleElement);
+
+        
+
+
+    }
 }
-console.log();
+displayWorks();
