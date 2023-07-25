dc3cfd63c6b22ef8855612020bba2afe31e0c124
Mélanie Fauchard <melaniefauchard0@gmail.com>
2023-07-25 13:55:43 +0200

    Modification des noms des variable suite à lasession de mentorat et cra de page de connexion

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,64 +1,75 @@
 // ------------------------------------------------------
 // Récupérer dynamiquement les projets de l'architecture.
 // ------------------------------------------------------
-
-const works = fetch("http://localhost:5678/api/works");
-works
- .then((response) => response.json())
- .then((works) => displayWorks(works))
- .catch((error) => {
-     console.log(`Erreur :`+ error);
-})
+const worksFetch = fetch("http://localhost:5678/api/works");
+worksFetch
+  .then((response) => response.json())
+  .then((works) => displayWorks(works))
+  .catch((error) => {
+    console.log(`Erreur :` + error);
+  });
 
 function displayWorks(works) {
-    for(let index = 0; index < works.length; index++) {
-        
-        const figure = works[index];
-        // Récupération de l'élément du Dom qui accueillera la galerie
-        const sectionGallery = document.querySelector(".gallery");
-        // Création d'une balise dédiée à une image de la gallerie
-        const worksElement = document.createElement("figure");
-        // Création des balises interne 
-        const imageElement = document.createElement("img");
-        imageElement.src = figure.imageUrl;
-        const titleElement = document.createElement("figcaption");
-        titleElement.innerHTML = figure.title;
-
-        // On ratache la balise figure à la section gallery
-        sectionGallery.appendChild(worksElement);
-        // On rattache les balises interne à la section figure
-        worksElement.appendChild(imageElement);
-        worksElement.appendChild(titleElement);
-    }
+  for (let index = 0; index < works.length; index++) {
+    const worksIndex = works[index];
+    // Récupération de l'élément du Dom qui accueillera la galerie
+    const sectionGallery = document.querySelector(".gallery");
+    // Création d'une balise dédiée à une image de la gallerie
+    const worksElement = document.createElement("figure");
+    // Création des balises interne
+    const imageElement = document.createElement("img");
+    imageElement.src = worksIndex.imageUrl;
+    const titleElement = document.createElement("h3");
+    titleElement.innerHTML = worksIndex.title;
+    // Lien entre la balise figure et la section gallery
+    sectionGallery.appendChild(worksElement);
+    // lien entre les balises interne à la section figure
+    worksElement.appendChild(imageElement);
+    worksElement.appendChild(titleElement);
+  }
 }
-displayWorks(works);
+// displayWorks(works);
 
-const categories = fetch("http://localhost:5678/api/categories");
-categories
- .then((response) => response.json())
- .then((categories) => console.log(categories))
- .catch((error) => {
-     console.log(`Erreur :`+ error);
-})
-// function displayCategory(categories) {
-//        for(let index = 0; index < categories.length; index++) {
-        
-//         const  sectionCategory = categories[index];
-//         // Création de l'élément du Dom qui accueillera les filtres
-//         const sectionFiltres = document.querySelector(".filtres");
-//         // Création d'une balise dédiée à un bouton filtre de la gallerie
-//         const categoryElement = document.createElement("section");
-//         // Création des balises interne 
-//         const buttonElement = document.createElement("button");
+// ------------------------------------------------------
+// Récupérer dynamiquement les categories de l'architecture.
+// ------------------------------------------------------
+const categoriesFetch = fetch("http://localhost:5678/api/categories");
+categoriesFetch
+  .then((response) => response.json())
+  .then((categories) => displayCategory(categories))
+  .catch((error) => {
+    console.log(`Erreur :` + error);
+  });
 
-//          // On ratache la balise figure à la section gallery
-//          sectionFiltres.appendChild(categoryElement);
-//          // On rattache les balises interne à la section figure
-//          categoryElement.appendChild(imagbuttonElementeElement);
-         
-//         }
-//         console.log(displayCategory);
-       
-// }
+function displayCategory(categories) {
+  for (let index = 0; index < categories.length; index++) {
+    const categoryIndex = categories[index];
+    // Création de l'élément du Dom qui accueillera les filtres
+    const sectionFiltres = document.querySelector(".filtres");
+    // Création d'une balise dédiée à un bouton filtre de la gallerie
+    const categoryElement = document.createElement("button");
+    categoryElement.classList.add("btnFilter");
+    categoryElement.innerHTML = categoryIndex.name;
+    // Lien entre la balise input et la section filtre
+    sectionFiltres.appendChild(categoryElement);
+
+    categoryElement.addEventListener("click", selectCategory);
+    categoryElement.dataset.blob = index + "-proot";
+  }
+}
 
 // displayCategory(categories);
+
+// si je clique sur une id categorie alors tu selectionne les travaux avec id categoryIndex
+// const categorySelected = document.querySelector(".btnFilter");
+
+// categorySelected.addEventListener('click', function() {
+//     const categoryFilter = categories.filter(function(categories) {
+//         return categories.id;
+//     });
+// })
+
+function selectCategory(event) {
+  event.target;
+  console.log(event.target.dataset.blob);
+}
