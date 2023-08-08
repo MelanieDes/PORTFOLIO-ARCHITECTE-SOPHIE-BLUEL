44d849b68debc46408cd14ba74f4bdd26ef11e6e
Mélanie Fauchard <melaniefauchard0@gmail.com>
2023-08-08 15:32:15 +0200

    Appel des travaux avec le bouton Tous

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -27,9 +27,8 @@ function displayWorks(works) {
     // lien entre les balises interne à la section figure
     worksElement.appendChild(imageElement);
     worksElement.appendChild(titleElement);
-  } 
+  }
 }
-
 
 // ------------------------------------------------------
 // Récupérer dynamiquement les categories de l'architecture.
@@ -45,79 +44,128 @@ fetch("http://localhost:5678/api/categories")
 
 function displayCategory(categories) {
   // Création de l'élément du Dom qui accueillera les filtres
-    const sectionFiltres = document.querySelector(".filtres");
-    
+  const sectionFiltres = document.querySelector(".filtres");
 
-    const buttonAll = document.createElement("button");
-    buttonAll.textContent = "Tous";
-    buttonAll.classList.add("btnFilter");
-    sectionFiltres.appendChild(buttonAll);
+  // Création du bouton Tous
+  const buttonAll = document.createElement("button");
+  buttonAll.textContent = "Tous";
+  buttonAll.classList.add("btnFilter");
+  sectionFiltres.appendChild(buttonAll);
 
-    let firstButtonsSelected = document.querySelector(".btnFilter");
-    firstButtonsSelected.classList.add("active");
-    
-    buttonAll.addEventListener("click", (event) => {
-      event.preventDefault();
-      setActiveButton(buttonAll);
+  // Mise en place de la class active sur le premier bouton
+  let firstButtonsSelected = document.querySelector(".btnFilter");
+  firstButtonsSelected.classList.add("active");
+
+  // Appel des travaux via le bouton Tous
+  buttonAll.addEventListener("click", (event) => {
+    event.preventDefault();
+    const sectionGallery = document.querySelector(".gallery");
+      sectionGallery.innerHTML = "";
+    fetch("http://localhost:5678/api/works")
+    .then((response) => response.json())
+    .then((works) => displayWorks(works))
+    .catch((error) => {
+      console.log(`Erreur :` + error);
+    });
+    setActiveButton(buttonAll);
   });
-    
-    for (let index = 0; index < categories.length; index++) {
-        const categoryIndex = categories[index];
-        
-        // Création d'une balise dédiée à un bouton filtre de la gallerie
-        const categoryElement = document.createElement("button");
-        categoryElement.classList.add("btnFilter");
-        categoryElement.innerHTML = categoryIndex.name;
-        // categoryElement.id = categoryIndex.id;
-        // categoryElement.dataset.blob = index + "-btn";
 
-        // Lien entre la balise input et la section filtre
-        sectionFiltres.appendChild(categoryElement);
-        
-        // Affichage des travaux selon bouton cliqué
-        categoryElement.addEventListener("click", (event) => {
-            event.preventDefault();
-            selectCategory(categoryIndex.id);
-            setActiveButton(categoryElement);
-        });
-        
-    }
+  for (let index = 0; index < categories.length; index++) {
+    const categoryIndex = categories[index];
+
+    // Création d'une balise dédiée à un bouton filtre de la gallerie
+    const categoryElement = document.createElement("button");
+    categoryElement.classList.add("btnFilter");
+    categoryElement.innerHTML = categoryIndex.name;
+    // categoryElement.id = categoryIndex.id; --> Début de recherche avec Robin
+    // categoryElement.dataset.blob = index + "-btn"; --> Début de recherche avec Robin
+
+    // Lien entre la balise input et la section filtre
+    sectionFiltres.appendChild(categoryElement);
+
+    // Affichage des travaux selon bouton cliqué
+    categoryElement.addEventListener("click", (event) => {
+      event.preventDefault();
+      selectCategory(categoryIndex.id);
+      setActiveButton(categoryElement);
+    });
+  }
 }
 
 function setActiveButton(button) {
   const buttons = document.querySelectorAll(".btnFilter");
-  buttons.forEach(btn => {
-    if(btn === button) {
-      btn.classList.add('active');
+  buttons.forEach((btn) => {
+    if (btn === button) {
+      btn.classList.add("active");
     } else {
-      btn.classList.remove('active');      
+      btn.classList.remove("active");
     }
-  })
+  });
 }
 
 // Fonction qui permet le tri des catégories
 function selectCategory(categoryId) {
   fetch("http://localhost:5678/api/works")
-  .then((response) => response.json())
-  .then((works) => {
-    // if(filtre == "tous") {
-    //   displayWorks(works);
-    // } else {
-      const selectWorks = works.filter((works) => works.categoryId === categoryId);
+    .then((response) => response.json())
+    .then((works) => {
+        const selectWorks = works.filter(
+        (works) => works.categoryId === categoryId
+      );
       const sectionGallery = document.querySelector(".gallery");
       sectionGallery.innerHTML = "";
-      displayWorks(selectWorks);
-    // }
-  })    
+      displayWorks(selectWorks);      
+    })
+    .catch((error) => {
+      console.log(`Erreur :` + error);
+    });
+  // console.log(event.target.dataset.blob); --> Début de recherche avec Robin
+}
+
+// ------------------------------------------------------
+//                GESTION DE LA MODALE.
+// ------------------------------------------------------
+
+let modal = null;
+
+const openModal = function (event) {
+  event.preventDefault();
+  const target = document.querySelector(event.target.getAttribute("href"));
+  target.style.display = null;
+  target.removeAttribute("aria-hidden");
+  target.setAttribute("aria-modal", "true");
+  modal = target;
+  modal.addEventListener("click", closeModal);
+  modal.querySelector(".btn-close").addEventListener("click", closeModal);
+  modal
+    .querySelector(".js-modal-stop")
+    .addEventListener("click", stopPropagation);
+};
+
+const closeModal = function (event) {
+  if (modal === null) return;
+  event.preventDefault();
+  modal.style.display = "none";
+  modal.setAttribute("aria-hidden", "true");
+  modal.removeAttribute("aria-modal");
+  modal.removeEventListener("click", closeModal);
+  modal.querySelector(".btn-close").removeEventListener("click", closeModal);
+  modal
+    .querySelector(".js-modal-stop")
+    .removeEventListener("click", stopPropagation);
+  modal = null;
+};
+
+const stopPropagation = function (event) {
+  event.stopPropagation();
+};
+
+document.querySelectorAll(".btn-recast").forEach((a) => {
+  a.addEventListener("click", openModal);
+});
+
+fetch("http://localhost:5678/api/works")
+  .then((response) => response.json())
+  .then((works) => displayWorks(works))
   .catch((error) => {
     console.log(`Erreur :` + error);
   });
-  // console.log(event.target.dataset.blob); --> Début de recerhce avec Robin
-}
-
-
-
-
-
-
-
